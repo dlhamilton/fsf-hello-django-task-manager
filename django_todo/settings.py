49104f57047939be4782b9bda352a932b8c4f034
Dan Hamilton <hamiltondan93@gmail.com>
2022-12-08 23:31:21 +0000

    models and admin created

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 
-if os.path.exists("envy.py"):
+if os.path.exists("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
